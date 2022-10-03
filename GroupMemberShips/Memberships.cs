2bc438b7d0ed9b72d973892509045923a204fab0
Ferdi Meijer <ferdi.meijer@rhdhv.com>
2022-10-03 09:25:19 +0200

    Added comment

--- GroupMemberShips/Memberships.cs
+++ GroupMemberShips/Memberships.cs
@@ -19,6 +19,14 @@ namespace GroupMemberShips
             return _memberships[id];
         }
 
+        /// <summary>
+        /// The enumerator returned from the dictionary is safe to use concurrently with
+        /// reads and writes to the dictionary, however it does not represent a 
+        /// moment-in-time snapshot of the dictionary.
+        /// The contents exposed through the enumerator may contain modifications made to
+        /// the dictionary after GetEnumerator was called.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerator<MembershipGroup> GetEnumerator()
         {
             return _memberships.Values.GetEnumerator();
